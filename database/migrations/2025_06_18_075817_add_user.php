2775e5abd401b2f1b6a61974581b3b5438560a74
izzulamirr <izzulamir.zulkifly@live.iium.edu.my>
2025-06-18 16:12:07 +0800

    Done Authorization

---
+++ database/migrations/2025_06_18_075817_add_user.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        schema::table('users', function (Blueprint $table) {
+            $table->string('nickname')->nullable();
+            $table->string('avatar')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('city')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
